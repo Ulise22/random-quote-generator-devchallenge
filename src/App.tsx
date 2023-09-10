37a8eed83744fd384a4a576a10b7b2b876489da2
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-09-10 20:10:07 -0300

    made the fetch of the quotes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 export default function App () {
+  const [quotes, setQuotes] = useState();
+
+  useEffect(() => {
+    fetch('https://quote-garden.onrender.com/api/v3/quotes')
+      .then(res => res.json())
+      .then(data => setQuotes(data))
+  }, [])
+
+  console.log(quotes)
   return(
     <main>
       <h1>Hola mundillo</h1>
