3148f51873ae42c64913a95c93d62175faf9d27a
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-09-12 10:51:45 -0300

    made the app initially work

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
+type Quote = {
+  quoteAuthor: string,
+  quoteGenre: string,
+  quoteText: string,
+  __v: number,
+  _id: string,
+}
+
 export default function App () {
-  const [quote, setQuote] = useState();
+  const [quote, setQuote] = useState<Quote>();
+  const [randomQuote, setRandomQuote] = useState(false)
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setLoading(true)
     fetch('https://quote-garden.onrender.com/api/v3/quotes/random')
       .then(res => res.json())
-      .then(data => {
-        setQuote(data.data[0]);
+        .then(data =>setQuote(data.data[0]))
+      .catch(() => console.log("Ha ocurrido un error"))
+      .finally(() => {
         setLoading(false);
+        setRandomQuote(false)
       })
-  }, [])
+  }, [randomQuote])
 
   console.log(quote)
-
-  if(loading) return <h1>loading...</h1>
   
    return(
     <main>
-      {/* <h1>{quote.quoteText}</h1> */}
+      {loading && <h1>Loading...</h1>}
+      <h1>{quote?.quoteText}</h1>
+      <button onClick={() => setRandomQuote(true)}>Random</button>
     </main>
   )
 }
