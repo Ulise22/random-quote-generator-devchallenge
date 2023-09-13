0a5a1a6189b57f47e51978bcd1a7fcf54946b644
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-09-13 11:13:22 -0300

    all app functionalities done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-
-type Quote = {
-  quoteAuthor: string,
-  quoteGenre: string,
-  quoteText: string,
-  __v: number,
-  _id: string,
-}
+import { Quote } from './components/types';
 
 export default function App () {
   const [quote, setQuote] = useState<Quote>();
+  const [authorQuotes, setAuthorQuotes] = useState<Quote[]>()
   const [randomQuote, setRandomQuote] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     setLoading(true)
@@ -26,13 +21,41 @@ export default function App () {
       })
   }, [randomQuote])
 
-  console.log(quote)
+  useEffect(() => {
+    fetch(`https://quote-garden.onrender.com/api/v3/quotes?author=${quote?.quoteAuthor}`)
+      .then(res => res.json())
+        .then(data => setAuthorQuotes(data.data))
+  },[quote])
+
+  //console.log(quote)
+  console.log(authorQuotes)
+
+  if(loading) return <h1>Loading...</h1>
   
-   return(
+  if(page == 0){ return(
     <main>
-      {loading && <h1>Loading...</h1>}
       <h1>{quote?.quoteText}</h1>
+      <div>
+        <p onClick={() => setPage(1)}> {quote?.quoteAuthor} </p>
+        <p> {quote?.quoteGenre} </p>
+      </div>
       <button onClick={() => setRandomQuote(true)}>Random</button>
     </main>
-  )
+  )} else {
+    return(
+      <main>
+        <button onClick={() => setPage(0)}>Random</button>
+        <h1> {quote?.quoteAuthor} </h1>
+        <ul>
+          { authorQuotes?.map(quote => {
+            return(
+              <li key={quote._id}>
+                <h2> {quote.quoteText} </h2>
+              </li>
+            )
+          }) }
+        </ul>
+      </main>
+    )
+  }
 }
---
+++ src/components/types.ts
@@ -0,0 +1,7 @@
+export type Quote = {
+    quoteAuthor: string,
+    quoteGenre: string,
+    quoteText: string,
+    __v: number,
+    _id: string,
+}
